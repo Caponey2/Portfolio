12900539b8077764aae9df5d517b8d1949b640eb
tannerhebert <tannerhebert20xx@gmail.com>
2023-01-13 22:23:36 -0800

    Confirmed Local Vercel Build GreenLight

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import WorkExperience from '../components/WorkExperience';
-import WorkExperience2 from '../components/WorkExperience2';
+
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
